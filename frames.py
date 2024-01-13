b2d0357b4a34a97c145edbca4c246eb56bbf8d0a
jai <sjai5803@gmail.com>
2024-01-13 23:13:21 +0530

    frames file updates

--- frames.py
+++ frames.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 
-class frame(ctk.CTKFrame):
+class frame(ctk.CTkFrame):
     def __init__(self, *args, header_name="Login", **kwargs):
         super().__init__(*args, **kwargs)
         
